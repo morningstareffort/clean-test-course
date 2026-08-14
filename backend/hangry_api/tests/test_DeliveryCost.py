defae1c0256bd85ce6038af1827b3c3109e47fdc
Harry Leatemia <harryleatemia@gmail.com>
2026-08-14 10:18:38 +0700

    lesson 3 function test

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -35,3 +35,17 @@ def test_LittleItems():
     cost = Delivery.calculate(order,delivery_distance)
   #Assert
     assert cost == 2.5
+
+
+
+
+
+    #Arrange
+order = MockSet()
+order.add(MockModel(quantity=3))
+order.add(MockModel(quantity=1))
+del_dist = 2
+#Act
+cost = Delivery.calculate(order, del_dist)
+#Assert
+assert cost == 2.50
